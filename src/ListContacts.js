efaa89b27d1f75fc05720a54b05c14929a7b6705
Manoj Talagadadeevi <manojt.854@gmail.com>
2017-07-30 17:29:29 -0400

    Filter and sort the contacts that are searched for | Use Destructuring to clean up code

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class ListContacts extends Component {
     static propTypes = {
@@ -16,9 +18,22 @@ class ListContacts extends Component {
     }
 
     render() {
+
+        const { contacts, onDeleteContact } = this.props;
+        const { query } = this.state;
+
+      let showingContacts;
+      if(query) {
+        const match = new RegExp(escapeRegExp(query), 'i');
+        showingContacts = contacts.filter((contact) => match.test(contact.name));
+      } else {
+        showingContacts = contacts;
+      }
+
+      showingContacts.sort(sortBy('name'));
+
       return (
         <div className="list-contacts">
-            {JSON.stringify(this.state)}
           <div className="list-contacts-top">
             <input
                 className="search-contacts"
@@ -30,7 +45,7 @@ class ListContacts extends Component {
           </div>
 
           <ol className="contact-list">
-              {this.props.contacts.map((contact) => (
+              {showingContacts.map((contact) => (
                   <li key={contact.id} className="contact-list-item">
                     <div className="contact-avatar" style={{
                         backgroundImage: `url(${contact.avatarURL})`
@@ -39,7 +54,7 @@ class ListContacts extends Component {
                       <p>{contact.name}</p>
                       <p>{contact.email}</p>
                     </div>
-                    <button onClick={() => this.props.onDeleteContact(contact)} className="contact-remove">
+                    <button onClick={() => onDeleteContact(contact)} className="contact-remove">
                       Remove
                     </button>
                   </li>
